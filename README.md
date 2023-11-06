49a4343875b11a1775130e74038a257fa89eacae
yzarc <jain.aj725@gmail.com>
2023-11-06 15:26:35 +0530

    Added a file and modified readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test-code
+
+## License and Subheader
---
+++ test.py
@@ -0,0 +1,7 @@
+print("This is a test file to test the git functionality")
+
+def foo(self):
+    print("This fucntion prints this line to stdout and returns string foo")
+    return "foo"
+
+print(foo())
