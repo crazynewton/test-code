3dfe733aacbbd09084079508ee12360d2ad90bc7
yzarc <jain.aj725@gmail.com>
2023-11-06 15:30:28 +0530

    Added another file
    A huge description

---
+++ test2.py
@@ -0,0 +1,7 @@
+print("This is another test file to test the git functionality")
+
+def foo2(self):
+    print("This fucntion prints this line to stdout and returns string foo2")
+    return "foo2"
+
+print(foo2())
